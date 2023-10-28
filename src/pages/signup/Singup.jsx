72eb96db607ef237bb76195c9195e4401e80c0ef
Kristjan Lääne <xonproject@gmail.com>
2023-10-28 19:53:03 +0300

    signup

--- src/pages/signup/Singup.jsx
+++ src/pages/signup/Singup.jsx
@@ -1,5 +1,49 @@
+import { useState } from 'react';
 import styles from './Signup.module.css';
 
 export const Singup = () => {
-  return <div>Singup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={styles['signup-form']}
+    >
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>
+          <span>displayname:</span>
+          <input
+            type='text'
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </span>
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
+  );
 };
