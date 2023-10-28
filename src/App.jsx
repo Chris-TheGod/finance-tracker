bc22691b855bc51a564e313a97159b9592c7a60b
Kristjan Lääne <xonproject@gmail.com>
2023-10-28 19:12:27 +0300

    router, pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+//Router
+import { ReactDOM } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Pages, components
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/Login';
+import { Singup } from './pages/signup/Singup';
+
 function App() {
-  return <>Hi</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+        <Route
+          path='login'
+          element={<Login />}
+        />
+        <Route
+          path='signup'
+          element={<Singup />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/signup/Singup.jsx
@@ -0,0 +1,5 @@
+import styles from './Signup.module.css';
+
+export const Singup = () => {
+  return <div>Singup</div>;
+};
