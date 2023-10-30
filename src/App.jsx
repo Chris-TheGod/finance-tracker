43cb0d1a8bdb79be0794f22d4d3bab39f58db75e
Kristjan Lääne <xonproject@gmail.com>
2023-10-30 18:12:35 +0200

    waiting until authj is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 //Router
 import { ReactDOM } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
 
 //Pages, components
 import { Home } from './pages/home/home';
@@ -9,24 +10,21 @@ import { Singup } from './pages/signup/Singup';
 import { Navbar } from './components/Navbar';
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='login'
-          element={<Login />}
-        />
-        <Route
-          path='signup'
-          element={<Singup />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <div className='App'>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Singup />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
   );
 }
 
