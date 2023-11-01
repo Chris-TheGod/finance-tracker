a00a7ab17872479b261a728b91d909210029504a
Kristjan Lääne <xonproject@gmail.com>
2023-11-01 19:44:16 +0200

    transaction form

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
 import styles from './Home.module.css';
+import { TransactionForm } from './TransactionForm';
 
 export const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 };
